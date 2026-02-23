45f04d3453ac9c9d7ee297b4256255c10b600e3a
Divyanshi Rathour <divyanshirathour2006@gmail.com>
2026-02-23 23:07:09 +0530

    Update simhash.py

--- Assignment-1/simhash.py
+++ Assignment-1/simhash.py
@@ -3,20 +3,16 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+# hash(s) = sum( s[i] * p^i ) mod m
+def word_hash(word):
+    p = 53
+    m = 2**64
+    h = 0
 
-# create 64-bit hash of a word
-def make_word_code(w):
-    mul = 53
-    limit = 2**64
-    code = 0
-    step = 1
+    for i in range(len(word)):
+        h = (h + ord(word[i]) * (p ** i)) % m
 
-    for ch in w:
-        code = (code + ord(ch) * step) % limit
-        step = (step * mul) % limit
-
-    return code
-
+    return h
 
 # get text and count words
 def fetch_words(link):
@@ -40,57 +36,6 @@ def fetch_words(link):
 
     return count_map
 
-
-# build 64-bit simhash
-def build_hash(word_map):
-    score = [0] * 64
-
-    for key in word_map:
-        freq = word_map[key]
-        code = make_word_code(key)
-
-        index = 0
-        while index < 64:
-            if (code >> index) & 1:
-                score[index] += freq
-            else:
-                score[index] -= freq
-            index += 1
-
-    result_hash = 0
-    pos = 0
-    while pos < 64:
-        if score[pos] > 0:
-            result_hash |= (1 << pos)
-        pos += 1
-
-    return result_hash
-
-# calculate matching bits
-def compare_hashes(a, b):
-    temp = a ^ b
-    diff_count = bin(temp).count("1")
-    return 64 - diff_count
-
-if len(sys.argv) != 3:
-    print("Usage: python simhash.py <URL1> <URL2>")
-    sys.exit()
-
-first = sys.argv[1]
-second = sys.argv[2]
-
-try:
-    words_first = fetch_words(first)
-    words_second = fetch_words(second)
-
-    hash_first = build_hash(words_first)
-    hash_second = build_hash(words_second)
-
-    print("\nSimhash 1:", hash_first)
-    print("Simhash 2:", hash_second)
-
-    same = compare_hashes(hash_first, hash_second)
-    print("\nMatching Bits:", same)
-
 except Exception as err:
+
     print("Error:", err)
